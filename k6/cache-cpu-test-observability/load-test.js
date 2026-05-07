d1d388f16755fdf255fa82abbaea448e988704e3
m.arunova <m.arunova@tinkoff.ru>
2026-05-07 11:28:18 +0300

    Task-11 Add kafka

--- k6/cache-cpu-test-observability/load-test.js
+++ k6/cache-cpu-test-observability/load-test.js
@@ -73,13 +73,10 @@ export function setup() {
         for (let userId of userIds) {
             for (let j = 0; j < likePerUser; j++) {
                 const randomPostId = postIds[Math.floor(Math.random() * postIds.length)];
-                const res = http.post(`${BASE_URL}/posts/${randomPostId}/likes?userId=${userId}`);
-                    headers: { 'Content-Type': 'application/json' },
-                });
+                const res = http.post(`${BASE_URL}/posts/${randomPostId}/likes?userId=${randomUserId}`);
 
                 if (res.status === 200 || res.status === 201) {
                     const post = JSON.parse(res.body);
-                    postIds.push(post.id);
                 }
             }
         }
